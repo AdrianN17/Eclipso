d99d82cb41c2b1970b66c218d68fb5c8af0e7241
Adrian <36133718+AdrianN17@users.noreply.github.com>
2019-04-24 18:48:56 -0500

    Nueva beta 3

---
+++ beta_3/libs/extra/extras.lua
@@ -0,0 +1,30 @@
+
+
+function collides(table,x,y)
+	if x> table.x and x<table.x+table.w and y>table.y and y<table.y+table.h then
+		return true
+	end
+end
+
+function enviar_data_jugador(obj,...)
+	local args={...}
+	local data={}
+
+	for _,arg in ipairs(args) do
+		data[arg]=obj[arg]
+	end
+
+	return data
+end
+
+function recibir_data_jugador(data,obj)
+	for name,datos in pairs(data) do
+   		obj[name]=datos
+	end
+end
+
+function collides_object(obj,x,y,w,h)
+	if obj.ox>x and obj.oy>y and obj.ox<w and obj.oy<h then
+		return true
+	end
+end
