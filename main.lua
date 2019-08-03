fd65f8969bfd59ee1a8052ded20be45e181e65cf
Adrian <36133718+AdrianN17@users.noreply.github.com>
2019-08-03 04:18:36 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,6 @@
 Gamestate = require "libs.hump.gamestate"
 Servidor = require "entidades.servidor"
+local socket = require "socket"
 
 function love.load()
 	
@@ -10,8 +11,17 @@ function love.load()
 	_G.py=love.physics
 
   	Gamestate.registerEvents()
-  	Gamestate.switch(Servidor,8,25,"acuaris","192.168.0.3")
+  	Gamestate.switch(Servidor,8,25,"acuaris",getIP())
 
+end
+
+function getIP()
+  local s = socket.udp()
+  s:setpeername("74.125.115.104",80)
+  local ip, _ = s:getsockname()
+  s:close()
+
+  return ip
 end
 
 --enviar imagenes de personajes del servidor al cliente
