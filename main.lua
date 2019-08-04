cc0b16c0564fc6741a7d4814a8329d9b0c09ed47
Adrian <36133718+AdrianN17@users.noreply.github.com>
2019-08-04 02:31:03 -0500

    Mejora II

--- main.lua
+++ main.lua
@@ -2,10 +2,13 @@ Gamestate = require "libs.hump.gamestate"
 Menu = require "escenas.menu"
 
 function love.load()
+
 	
 	
 	_G.lg=love.graphics
 	_G.lm=love.math
+	_G.lm.setRandomSeed(love.timer.getTime())
+	_G.seed = lm.getRandomSeed()
 	--_G.font = font
 	_G.py=love.physics
 
