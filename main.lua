67c70e6a6b60f8e9998c09c42ed3670c2afed818
Adrian <36133718+AdrianN17@users.noreply.github.com>
2019-08-03 22:34:56 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ function love.load()
 	_G.py=love.physics
 
   	Gamestate.registerEvents()
-  	Gamestate.switch(Servidor,8,25,"acuaris","192.168.0.3")
+  	Gamestate.switch(Servidor,8,25,"acuaris","0.0.0.0")
 
 end
 
